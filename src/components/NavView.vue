a06a686ef6c638e55570eb919e168aac803af8ad
achoule3 <alban.choulet@edu.univ-fcomte.fr>
2025-10-01 16:13:55 +0200

    avancement barre de nav

--- src/components/NavView.vue
+++ src/components/NavView.vue
@@ -1,8 +1,14 @@
 <template>
-    <div class="barre-nav">
-      ureinvirj
-      <img src="../images/logo-blanc.png" alt="logo" id="logo">
-    </div>
+    <nav class="barre-nav">
+      <img src="../images/logo.png" alt="logo" id="logo">
+      <div class="log">
+        <span>
+          <img src="../images/france.png" alt="france" class="drapeau"> /
+          <img src="../images/grande-bretagne.png" alt="grande-bretagne" class="drapeau">
+        </span>
+        <span><strong>Se connecter / S'identifier</strong></span>
+      </div>
+    </nav>
 
 
 </template>
@@ -22,7 +28,25 @@ body{
 .barre-nav{
   padding: 0;
   background-color: #00167a;
+  display: flex;
+  justify-content: space-between; /*space-around*/
+  color: white;
+  font-size: 1.2em;
 }
+#logo{
+  height: 100px;
+}
+.log{
+  align-content: center;
+}
+.log span{
+  padding: 0px 25px;
+}
+.drapeau{
+  height: 25px;
+  width: 40px;
+}
+
 </style>
 
 <script setup>
