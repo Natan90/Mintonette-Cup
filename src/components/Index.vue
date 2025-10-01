ea0561e8e73f71d8e06d47fa0ad2baf5682c377f
Noa Bigeard <noa.bigeard@edu.univ-fcomte.fr>
2025-10-01 19:10:56 +0200

    Tests pour le futur

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,8 +1,20 @@
 <template>
     <div class="image">
-        <img src="../images/PhotoFond.jpeg" alt="">
+        <img src="../images/Photo.webp" alt="">
+      <div class="texteImage">
+        Montpellier accueille la Mintonette Cup, le tournoi mondial de volley-ball qui rassemble les meilleurs joueurs !<br>
+        changer l'image ( pas les bonnes couleurs )
+      </div>
+
     </div>
-  
+  <div class="hover">
+    <p>Hover moi </p>
+    <div class="TuasHover">
+      <p> Bravo tu as hover ca Chef ! </p>
+
+    </div>
+  </div>
+
 
 </template>
 
@@ -14,11 +26,43 @@ import NavView from "@/components/NavView.vue";
 
 
 <style>
-    .image{
-
+    body{
+    }
+    .image {
     }
     .image img{
-         width: 100%;
+        width: 100%;
         height: 100%;
+        margin-top: 1.5em;
+
     }
+
+    .texteImage {
+      font-size: 25px;
+      text-align: center;
+      align-items: center;
+      justify-content: center;
+      border: solid black 1px;
+      border-radius: 10px;
+      padding: 5px 10px;
+      background: rgba(255, 255, 255, 0.4);
+    }
+
+    .hover p{
+      border: solid black 1px;
+      border-radius: 10px;
+      padding: 5px 10px;
+      max-width: 70px;
+    }
+    .hover p:hover{
+      max-width: 500px;
+
+    }
+    .TuasHover{
+      display: none;
+    }
+    .hover:hover .TuasHover {
+      display: block;
+    }
+
 </style>
