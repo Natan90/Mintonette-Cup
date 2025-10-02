fc6a6a58ff6ba77ffd2740a921c6772c405eca63
achoule3 <alban.choulet@edu.univ-fcomte.fr>
2025-10-02 15:01:18 +0200

    le truc que Natan veut plus

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
-<!--  <NavView></NavView>-->
   <Index></Index>
 
 </template>
 
 <script setup>
-import NavView from "@/components/NavView.vue";
 import Index from "./components/Index.vue";
 
 
