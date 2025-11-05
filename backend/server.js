fd7452af382709e5e706bc1883bfc773efd3b46f
achoule3 <alban.choulet@edu.univ-fcomte.fr>
2025-11-05 19:08:48 +0100

    petite modifs

--- backend/server.js
+++ backend/server.js
@@ -23,22 +23,7 @@ app.use("/utilisateur", authRoutes);
 const swaggerSpec = require('./swagger');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// Test route
-app.get("/api/health", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT NOW()");
-    res.json({
-      status: "ok",
-      database: "connected",
-      time: result.rows[0].now,
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "error",
-      message: error.message,
-    });
-  }
-});
+
 
 // Lancement du serveur
 app.listen(PORT, () => {
