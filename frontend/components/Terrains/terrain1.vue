ef68362400e708b8e02bdd74ae3bd1759a1d913c
Noa Bigeard <noa.bigeard@edu.univ-fcomte.fr>
2025-11-12 11:45:26 +0100

    Petit joueurs sur le petit terrain

--- frontend/components/Terrains/terrain1.vue
+++ frontend/components/Terrains/terrain1.vue
@@ -1,58 +1,61 @@
 <template>
   <NavView></NavView>
 
-  <router-link to="/Terrains/terrain_4" class="btnLink"
-    >terrain précedent</router-link
-  >
-  <router-link to="/Terrains/terrain_2" class="btnLink"
-    >terrain suivant</router-link
-  >
-  <br /><br /><br /><br />
+  <router-link to="/Terrains/terrain_4" class="btnLink">
+    terrain précédent
+  </router-link>
+  <router-link to="/Terrains/terrain_2" class="btnLink">
+    terrain suivant
+  </router-link>
+
   <div class="terrainConteneur">
     <div class="image">
-      <img src="../../images/TerrainUnique.png" alt="Image terrain Unique " />
-    </div>
-    <div class="LogoPersonne">
-      <img
-        v-for="index in nombreDeJoueur"
-        :key="index"
-        src="../../images/LogoPersonne.png"
-        alt="personne" />
-    </div>
-    <div class="LogoPersonne2">
-      <img
-        v-for="index in nombreDeJoueur"
-        :key="index"
-        src="../../images/LogoPersonne.png"
-        alt="personne" />
+      <div class="terrain">
+        <div class="equipe gauche">
+          <img v-for="i in 6" :key="'g'+i" src="../../images/LogoPersonne.png" alt="personne" />
+        </div>
+
+        <div class="equipe droite">
+          <img v-for="i in 6" :key="'d'+i" src="../../images/LogoPersonne.png" alt="personne" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
 import NavView from "@/components/NavView.vue";
-import { ref } from "vue";
-const nombreDeJoueur = ref(6);
 </script>
-
-<style></style>
 
 <style scoped>
 .image {
   width: 75%;
+  aspect-ratio: 16/9;
   margin: auto;
+  background-image: url("../../images/TerrainUnique.png");
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  position: relative;
 }
-.LogoPersonne {
-  max-width: 210px;
-  transform: rotate(-90deg);
-  margin-left: 650px;
+
+.terrain {
+  position: absolute;
+  inset: 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
 }
-.LogoPersonne2 {
-  max-width: 210px;
-  transform: rotate(90deg);
+
+.equipe {
+  display: grid;
+  grid-template-rows: repeat(3, 1fr);
+  grid-template-columns: repeat(2, 1fr);
+  justify-items: center;
+  align-items: center;
 }
-.LogoPersonne,
-.LogoPersonne2 img {
-  width: 55px;
+
+.equipe img {
+  width: 80px;
+  height: 80px;
 }
 </style>
