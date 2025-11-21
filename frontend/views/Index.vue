f85cc505a05e7db6a1a2c6d39dbadf6867fbf74e
natan bouche <natan.bouche@edu.univ-fcomte.fr>
2025-11-21 14:38:48 +0100

    page d'accueil

--- frontend/views/Index.vue
+++ frontend/views/Index.vue
@@ -1,127 +1,133 @@
 <template>
-  <div>
-    <NavView :style="{ top: navbar }" class="navbar" />
+  <NavView :style="{ top: navbar }" class="navbar" />
 
-    <div class="image">
-      <img src="../images/photo_fond.png" alt="" />
-      <div class="texteImage">
+  <div class="image">
+    <div class="texteImage">
       Mintonette Cup
-        <!-- {{ $t("mintonetteCup.title") }} -->
-      </div>
+      <!-- {{ $t("mintonetteCup.title") }} -->
     </div>
+  </div>
 
-    <Map> </Map>
-
-    <!-- <TableauMatchs></TableauMatchs> -->
+  <div class="fondBlanc">
+    <Map></Map>
 
     <section class="infos">
-      <!--Ca c'est pour le truc de Natan que je n'ai pas compris (quand il veut hover un truc ca agrandi pour mettre plus de texte je ne sais pas quoi la ...)-->
-
-<!-- #########################################################################SI C'EST POSSIBLE, FAUDRAIT QUE CA COMMENCE A AUGMENTER LORSQU'ON VOIT LES NOMBRES ##########################################################################-->
 
       <div class="bloc">
+        <img class="illustration" src="./../images/logo-couleur.png" />
+
+        <div class="contenuTexte">
           <span class="title">3ème édition</span>
-          <img class="illustration" src="./../images/logo-couleur.png" />
+
           <span class="descri">
-              La Mintonette Cup, désormais accueillie à
-              <strong>Montpellier</strong>, connaît un essor remarquable et
-              s’impose comme l’un des rendez-vous majeurs du volley amateur dans
-              le Sud de la France. 
-            
-              L’édition précédente a rassemblé près de
-              <strong>15 000 spectateurs sur trois jours</strong>, portée par
-              l’attractivité des installations montpelliéraines et la forte
-              culture volley de la ville, notamment grâce à la présence du
-              <strong>Montpellier Hérault Sport Club Volley-Ball</strong>.
+            La Mintonette Cup, désormais accueillie à <strong>Montpellier</strong>, connaît un essor remarquable
+            et s’impose comme l’un des rendez-vous majeurs du volley amateur dans le Sud de la France.
+            L’édition précédente a rassemblé près de
+            <strong>15 000 spectateurs sur trois jours</strong>, portée par l’attractivité des installations
+            montpelliéraines et la forte culture volley de la ville, notamment grâce au
+            <strong>Montpellier Hérault Sport Club Volley-Ball</strong>.
           </span>
+
           <div class="voirPlus">
-              <span class="pointer">Voir plus</span>
+            <span class="pointer">Voir plus</span>
           </div>
+        </div>
       </div>
 
       <div class="bloc">
-          <span class="title"><countUp :from="0" :to="760" :duration="2" class="count-up-text" /> millions</span>
-          <img class="illustration" src="./../images/photo_fond.png" />
-          <span class="descri">
-            C'est le nombre  de personnes pratiquants le volley-ball, qu’il s’agisse de
-            loisirs, de pratique scolaire ou à haut niveau. Ce chiffre fait du
-            volley-ball l’un des sports collectifs <strong>les plus populaires au monde</strong>,
-            juste derrière le football.
+        <img class="illustration" src="./../images/photo_fond.png" />
+
+        <div class="contenuTexte">
+          <span class="title">
+            <countUp :from="0" :to="760" :duration="2" class="count-up-text" /> millions
           </span>
-          <div class="voirPlus">
-              <span class="pointer">Voir plus</span>
-          </div>
-      </div>
-      
-      <div class="bloc">
-          <span class="title"><countUp :from="0" :to="200" :duration="2" class="count-up-text" /> pays</span>
-          <img class="illustration" src="./../images/logo-couleur.png" />
+
           <span class="descri">
-            C'est le nombre de pays oû le volley-ball est aujourd’hui représenté
-             à travers divers clubs,
-            compétitions et fédérations nationales. La Fédération Internationale
-            de Volley-ball (FIVB) regroupe
-            <strong>plus de 220 fédérations membres</strong>, montrant l’ampleur
-            mondiale du sport et son implantation dans toutes les régions du
-            globe.
+            C'est le nombre de personnes pratiquant le volley-ball, qu’il s’agisse de loisirs, de pratique
+            scolaire ou à haut niveau. Ce chiffre fait du volley-ball l’un des sports collectifs
+            <strong>les plus populaires au monde</strong>, juste derrière le football.
           </span>
+
           <div class="voirPlus">
-              <span class="pointer">Voir plus</span>
+            <span class="pointer">Voir plus</span>
           </div>
+        </div>
       </div>
 
       <div class="bloc">
-          <span class="title"><countUp :from="0" :to="24" :duration="2" class="count-up-text" /> millions</span>
-          <img class="illustration" src="./../images/logo-couleur.png" />
-          <span class="descri">
-            C’est le pic d’audience enregistré pour un match de volley-ball en
-            2024, preuve de l’intérêt croissant du public pour ce sport. Les
-            compétitions internationales, en particulier, attirent
-            <strong>des millions de téléspectateurs</strong> et contribuent à
-            renforcer la visibilité du volley sur la
-            <strong>scène sportive mondiale</strong>.
+        <img class="illustration" src="./../images/logo-couleur.png" />
+
+        <div class="contenuTexte">
+          <span class="title">
+            <countUp :from="0" :to="200" :duration="2" class="count-up-text" /> pays
           </span>
+
+          <span class="descri">
+            C'est le nombre de pays où le volley-ball est aujourd’hui représenté à travers divers clubs,
+            compétitions et fédérations nationales. La Fédération Internationale de Volley-ball (FIVB) regroupe
+            <strong>plus de 220 fédérations membres</strong>, montrant l’ampleur mondiale du sport et son implantation.
+          </span>
+
           <div class="voirPlus">
-              <span class="pointer">Voir plus</span>
+            <span class="pointer">Voir plus</span>
           </div>
+        </div>
       </div>
 
+      <!-- ----- BLOC 4 ----- -->
       <div class="bloc">
+        <img class="illustration" src="./../images/logo-couleur.png" />
+
+        <div class="contenuTexte">
+          <span class="title">
+            <countUp :from="0" :to="24" :duration="2" class="count-up-text" /> millions
+          </span>
+
+          <span class="descri">
+            C’est le pic d’audience enregistré pour un match de volley-ball en 2024, preuve de l’intérêt
+            croissant du public pour ce sport. Les compétitions internationales attirent
+            <strong>des millions de téléspectateurs</strong> et renforcent la visibilité du volley.
+          </span>
+
+          <div class="voirPlus">
+            <span class="pointer">Voir plus</span>
+          </div>
+        </div>
+      </div>
+
+      <!-- ----- BLOC 5 ----- -->
+      <div class="bloc">
+        <img class="illustration" src="./../images/logo-couleur.png" />
+
+        <div class="contenuTexte">
           <span class="title">Les réseaux sociaux n’ont pas été en reste</span>
-          <img class="illustration" src="./../images/logo-couleur.png" />
+
           <span class="descri">
             La couverture numérique de la Mintonette Cup a généré
             <strong>1,2 million d’impressions</strong> et plus de
-            <strong>150 000 interactions</strong>, alimentées par des vidéos
-            courtes, des moments forts des matchs et des interviews exclusives.
-            Forts de cette dynamique, les organisateurs visent pour la prochaine
-            édition de dépasser les
-            <strong>300 000 visionnages en ligne</strong>, consolidant ainsi la
-            réputation de la Mintonette Cup comme un événement incontournable à
-            Montpellier.
+            <strong>150 000 interactions</strong>.
+            Les organisateurs visent pour la prochaine édition de dépasser
+            <strong>300 000 visionnages en ligne</strong>, renforçant encore sa notoriété.
           </span>
+
           <div class="voirPlus">
-              <span class="pointer">Voir plus</span>
+            <span class="pointer">Voir plus</span>
           </div>
+        </div>
       </div>
 
     </section>
 
-  </div>
-  <!-- <router-link to="/PolygoneCreation" class="btnLink"
-    >Truc pour les polygones</router-link
-  >
-  <br /><br /> -->
+    <div class="presta_formulaire">
+      <ListPresta></ListPresta>
+      <Formulaire></Formulaire>
+    </div>
 
-  <div class="presta_formulaire">
-    <ListPresta></ListPresta>
-    <Formulaire></Formulaire>
   </div>
-
-  <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
 
   <Footer></Footer>
 </template>
+
 
 <script setup>
 import NavView from "@/components/NavView.vue";
@@ -152,6 +158,7 @@ onBeforeUnmount(() => {
 });
 </script>
 
+
 <style>
 body::-webkit-scrollbar {
   display: none;
@@ -160,7 +167,7 @@ body::-webkit-scrollbar {
 
 <style scoped>
 
-.pointer{    
+.pointer {
   cursor: pointer;
 }
 
@@ -173,10 +180,31 @@ body::-webkit-scrollbar {
   z-index: 999;
 }
 
-.image img {
+.image {
+  position: fixed;
+  top: 0;
+  left: 0;
   width: 100%;
-  height: 100%;
-  /* margin-top: 1.5em; */
+  height: 100vh;
+  background-image: url("../images/photo_fond.png");
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+  filter: brightness(0.7);
+  z-index: -1;
+}
+
+.fondBlanc {
+  width: 90%;
+  margin: auto;
+  background: white;
+  position: relative;
+  margin-top: 100vh;
+  padding-top: 50px;
+  z-index: 10;
+  border-radius: 20px;
+  box-shadow: 0 -10px 20px rgba(0,0,0,0.2);
+  padding-bottom: 10%;
 }
 
 .texteImage {
@@ -195,60 +223,77 @@ body::-webkit-scrollbar {
 
 .infos {
   display: flex;
-  justify-content: space-evenly;
-  flex-direction: row;
-  flex-wrap: wrap;
-  align-items: center;
-
-  width: 95%;
-  margin: 50px 2.5% 50px 2.5%;
-  gap: 50px;
+  flex-direction: column;
+  gap: 80px;
+  width: 90%;
+  margin: 50px auto;
 }
 
 .bloc {
-    display: flex;
-    flex-wrap: wrap;
-    background-color: var(--jaune-logo);
-    opacity: 0.7;
-    border: solid black 1px;
-    padding: 5px 10px;
-    max-width: 60%;
-    width: 300px;
-
-    transition: var(--transition-fast);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 40px;
 }
 
-.bloc:hover {
-    background-color: var(--jaune-logo);
-    opacity: 0.9;
-    transform: scale(1.1);
-    box-shadow: 0 8px 20px rgba(0, 0, 0, 0.2);
-}
-
-.bloc .title {
-    font-weight: bold;
-    font-size: 1.3em;
-    transition: font-size 0.3s ease;
+.bloc:nth-child(even) {
+  flex-direction: row-reverse;
 }
 
 .illustration {
-    width: 90%;
-    margin: 20px 5%;
-    background-color: white;
+  width: 40%;
+  object-fit: cover;
+  border-radius: 15px;
+  box-shadow: 0 8px 25px rgba(0,0,0,0.15);
+}
+
+.contenuTexte {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  max-width: 600px;
+}
+
+.title {
+  font-weight: 700;
+  font-size: 2em;
+  color: var(--bleu-logo);
 }
 
 .descri {
-    font-size: 0.7em;
+  font-size: 1.1em;
+  line-height: 1.6;
 }
 
 .voirPlus {
+  color: var(--bleu-logo);
+  font-weight: bold;
+  cursor: pointer;
+  text-decoration: underline;
+}
+
+@media (max-width: 900px) {
+  .bloc,
+  .bloc:nth-child(even) {
+    flex-direction: column;
+  }
+
+  .illustration {
     width: 100%;
-    display: flex;
-    justify-content: flex-end;
-    text-align: end;
-    color: var(--bleu-logo);
-    font-weight: bold;
-    text-decoration: underline;
+  }
+
+  .contenuTexte {
+    text-align: center;
+    max-width: 100%;
+  }
+
+  .title {
+    font-size: 1.7em;
+  }
+
+  .descri {
+    font-size: 1em;
+  }
 }
 
 .presta_formulaire {
